03f391c22a1b08ee75dfe71e095cd99d046c2dd5
vanyauhalin <vanyauhalin@gmail.com>
2022-10-02 17:00:55 +0400

    fix: remove viewModel property in SpaceEvenlyGridColumnView

--- RoutineJournal/Utilities/SpaceEvenlyGridColumnView.swift
+++ RoutineJournal/Utilities/SpaceEvenlyGridColumnView.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
 struct SpaceEvenlyGridColumnView<Content>: View where Content: View {
-  let gridViewModel: SpaceEvenlyGridViewModel
+  let viewModel: SpaceEvenlyGridViewModel
   @ViewBuilder var content: () -> Content
 
   var body: some View {
     content()
       .readSize { size in
-        gridViewModel.setColumnWidth(width: size.width)
+        viewModel.setColumnWidth(width: size.width)
       }
   }
 }
