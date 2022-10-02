4e1efcddae5cee8f31b5c97f84bf53788afc8f89
vanyauhalin <vanyauhalin@gmail.com>
2022-10-02 18:42:05 +0400

    feat: move CalendarViewModel initializations

--- RoutineJournal/Views/CalendarViewModel.swift
+++ RoutineJournal/Views/CalendarViewModel.swift
@@ -1,7 +1,16 @@
 import Foundation
 
-struct CalendarViewModel {
+class CalendarViewModel {
   let calendar: Calendar
+  let today: Date
+
+  init(_ calendar: Calendar, today: Date) {
+    self.calendar = calendar
+    self.today = today
+  }
+}
+
+extension CalendarViewModel {
   var weekdaySymbols: [String] {
     let defaultSymbols = calendar.veryShortWeekdaySymbols
     if calendar.firstWeekday == 1 {
@@ -10,8 +19,22 @@ struct CalendarViewModel {
     let bound = calendar.firstWeekday - 1
     return Array(defaultSymbols[bound...] + defaultSymbols[..<bound])
   }
+}
 
-  init(_ calendar: Calendar) {
-    self.calendar = calendar
+extension CalendarViewModel {
+  var month: CalendarMonth {
+    CalendarMonth(calendar, anchor: today)
+  }
+
+  var monthViewModel: CalendarMonthViewModel {
+    CalendarMonthViewModel(
+      calendar,
+      month: month,
+      columnCount: weekdaySymbols.count
+    )
+  }
+
+  var monthsViewModel: CalendarMonthsViewModel {
+    CalendarMonthsViewModel(calendar, monthViewModel: monthViewModel)
   }
 }
