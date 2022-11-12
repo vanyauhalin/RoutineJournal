3d0af56e4ee44c0b375d0d0e765b4900d10037b0
vanyauhalin <vanyauhalin@gmail.com>
2022-11-12 15:36:38 +0400

    refactor: delete unnecessary label view

--- Timeline/TimelineJEvent/TimelineJEventView.swift
+++ Timeline/TimelineJEvent/TimelineJEventView.swift
@@ -5,33 +5,35 @@ public struct TimelineJEventView: View {
 
   public var body: some View {
     TimelineItemContainerView(backgroundColor: viewModel.backgroundColor) {
-      TimelineItemLabelView(
+      Label(
         title: {
-          VStack(alignment: .leading, spacing: .zero) {
-            TimelineItemTitleView(
-              title: viewModel.event.title,
-              foregroundColor: viewModel.titleForegroundColor
-            )
-            if let notes = viewModel.event.notes {
-              TimelineItemNotesPreview(
-                notes: notes,
-                foregroundColor: viewModel.notesForegroundColor
+          HStack(spacing: .zero) {
+            VStack(alignment: .leading, spacing: .zero) {
+              TimelineItemTitleView(
+                title: viewModel.event.title,
+                foregroundColor: viewModel.titleForegroundColor
               )
+              if let notes = viewModel.event.notes {
+                TimelineItemNotesPreview(
+                  notes: notes,
+                  foregroundColor: viewModel.notesForegroundColor
+                )
+              }
             }
-          }
-          Spacer(minLength: 8)
-          VStack(alignment: .trailing, spacing: .zero) {
-            TimelineItemTimeView(
-              date: viewModel.event.startDate,
-              foregroundColor: viewModel.startDateForegroundColor
-            )
-            if let endDate = viewModel.event.endDate {
+            Spacer(minLength: 8)
+            VStack(alignment: .trailing, spacing: .zero) {
               TimelineItemTimeView(
-                date: endDate,
-                foregroundColor: viewModel.endDateForegroundColor
+                date: viewModel.event.startDate,
+                foregroundColor: viewModel.startDateForegroundColor
               )
-            } else {
-              Spacer()
+              if let endDate = viewModel.event.endDate {
+                TimelineItemTimeView(
+                  date: endDate,
+                  foregroundColor: viewModel.endDateForegroundColor
+                )
+              } else {
+                Spacer()
+              }
             }
           }
         },
