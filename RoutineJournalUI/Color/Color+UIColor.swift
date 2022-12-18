78bda7460dd7e82aebace9cf6fa89b53db527aa6
vanyauhalin <vanyauhalin@gmail.com>
2022-12-18 14:33:02 +0400

    Extend `Color` with the `UIColor.systemGroupedBackground` property

--- RoutineJournalUI/Color/Color+UIColor.swift
+++ RoutineJournalUI/Color/Color+UIColor.swift
@@ -1,7 +1,9 @@
+// swiftlint:disable line_length
 import SwiftUI
 
 extension Color {
   public static let label = Color(UIColor.label)
   public static let placeholderText = Color(UIColor.placeholderText)
   public static let systemGray = Color(UIColor.systemGray)
+  public static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
 }
