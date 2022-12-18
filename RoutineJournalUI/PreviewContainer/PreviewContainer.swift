f08838bf2cb33f24925db1d319d01b96b4687e1d
vanyauhalin <vanyauhalin@gmail.com>
2022-12-18 17:11:27 +0400

    Add `PreviewContainer` to position `Mirror` in other previews

---
+++ RoutineJournalUI/PreviewContainer/PreviewContainer.swift
@@ -0,0 +1,69 @@
+#if DEBUG
+import SwiftUI
+
+public struct PreviewContainer<Content, Mirror>: View
+where Content: View, Mirror: View {
+  public let position: PreviewContainerPosition
+  public let content: Content
+  public let mirror: Mirror
+
+  public var top: Bool {
+    position.0 == .top
+  }
+  public var bottom: Bool {
+    position.0 == .bottom
+  }
+  public var leading: Bool {
+    position.1 == .leading
+  }
+  public var trailing: Bool {
+    position.1 == .trailing
+  }
+
+  public var body: some View {
+    ZStack {
+      content
+      VStack {
+        if bottom {
+          Spacer()
+        }
+        HStack {
+          if trailing {
+            Spacer()
+          }
+          mirror
+          if leading {
+            Spacer()
+          }
+        }
+        if top {
+          Spacer()
+        }
+      }
+    }
+  }
+
+  public init(
+    position: PreviewContainerPosition = (.top, .leading),
+    @ViewBuilder content: () -> Content,
+    @ViewBuilder mirror: () -> Mirror
+  ) {
+    self.position = position
+    self.content = content()
+    self.mirror = mirror()
+  }
+}
+
+struct PreviewContainer_Previews: PreviewProvider {
+  static var previews: some View {
+    PreviewContainer(
+      content: {
+        Text("content")
+      },
+      mirror: {
+        Text("mirror")
+      }
+    )
+  }
+}
+#endif
