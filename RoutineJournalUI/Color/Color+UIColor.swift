06c8bbd631afd241792be74148091c5d1a17991f
vanyauhalin <vanyauhalin@gmail.com>
2022-12-18 14:31:41 +0400

    Extend `Color` with the `UIColor.systemGray` property

--- RoutineJournalUI/Color/Color+UIColor.swift
+++ RoutineJournalUI/Color/Color+UIColor.swift
@@ -3,4 +3,5 @@ import SwiftUI
 extension Color {
   public static let label = Color(UIColor.label)
   public static let placeholderText = Color(UIColor.placeholderText)
+  public static let systemGray = Color(UIColor.systemGray)
 }
