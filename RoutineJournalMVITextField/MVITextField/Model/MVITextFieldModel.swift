63da7fb6079e01ee506e544ab364f15499411f3f
vanyauhalin <vanyauhalin@gmail.com>
2022-12-20 21:03:41 +0400

    Add `RoutineJournalMVITextField` to use as a basis for other fields

---
+++ RoutineJournalMVITextField/MVITextField/Model/MVITextFieldModel.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public protocol MVITextFieldModel: AnyObject {
+  var title: String { get }
+  var text: Binding<String> { get }
+
+  init()
+  init(text: Binding<String>)
+
+  func reinit(text: Binding<String>?) -> Self
+}
+
+extension MVITextFieldModel {
+  public init() {
+    self.init(text: .constant(.default))
+  }
+
+  public func reinit(text: Binding<String>? = nil) -> Self {
+    Self(text: text ?? self.text)
+  }
+}
---
+++ RoutineJournalMVITextField/MVITextField/View/MVITextField.swift
@@ -0,0 +1,58 @@
+import RoutineJournalCore
+import RoutineJournalUI
+import SwiftUI
+
+public struct MVITextField<Model>: SwiftUI.View where Model: MVITextFieldModel {
+  public typealias View = MVITextField
+
+  private let model: Model
+
+  public var body: some SwiftUI.View {
+    TextField(model.title, text: model.text)
+  }
+
+  public init(model: Model) {
+    self.model = model
+  }
+
+  public static func render() -> View {
+    let model = Model()
+    return View(model: model)
+  }
+
+  public func text(_ text: Binding<String>) -> View {
+    let model = model.reinit(text: text)
+    return View(model: model)
+  }
+}
+
+struct MVITextField_Previews: PreviewProvider {
+  typealias ExampleField = MVITextField<ExampleFieldModel>
+
+  final class ExampleFieldModel: MVITextFieldModel {
+    let title = "Example Field"
+    let text: Binding<String>
+
+    init(text: Binding<String>) {
+      self.text = text
+    }
+  }
+
+  struct PreviewContainer: View {
+    @State private var text = String.default
+
+    var body: some View {
+      PreviewBinding($text) {
+        Form {
+          ExampleField
+            .render()
+            .text($text)
+        }
+      }
+    }
+  }
+
+  static var previews: some View {
+    PreviewContainer()
+  }
+}
