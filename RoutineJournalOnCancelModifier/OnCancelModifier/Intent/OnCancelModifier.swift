48cfdf5ae08a383a4cb6204a0e110cd30e6d04b4
vanyauhalin <vanyauhalin@gmail.com>
2022-12-29 01:52:22 +0400

    Add `OnCancelModifier` `onCancel` method with default implementation

--- RoutineJournalOnCancelModifier/OnCancelModifier/Intent/OnCancelModifier.swift
+++ RoutineJournalOnCancelModifier/OnCancelModifier/Intent/OnCancelModifier.swift
@@ -4,9 +4,11 @@ public protocol OnCancelModifier: MVIIntent {
   var actionOnCancel: () -> Void { get set }
 
   func reinit(model: Model, actionOnCancel: @escaping () -> Void) -> Self
+
+  func onCancel()
 }
 
-extension CancelModifier {
+extension OnCancelModifier {
   public func reinit(
     model: Model,
     actionOnCancel: @escaping () -> Void
@@ -14,5 +16,9 @@ extension CancelModifier {
     let intent = reinit(model: model)
     intent.actionOnCancel = actionOnCancel
     return intent
+  }
+
+  public func onCancel() {
+    actionOnCancel()
   }
 }
