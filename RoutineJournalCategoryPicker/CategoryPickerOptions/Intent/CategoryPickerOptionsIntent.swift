6a20e4fa2aa2fb346cc65f4acd6fd8d3150237e7
vanyauhalin <vanyauhalin@gmail.com>
2022-12-29 02:26:57 +0400

    Make `actionOnSelect` in `CategoryPicker` modules optional

--- RoutineJournalCategoryPicker/CategoryPickerOptions/Intent/CategoryPickerOptionsIntent.swift
+++ RoutineJournalCategoryPicker/CategoryPickerOptions/Intent/CategoryPickerOptionsIntent.swift
@@ -4,9 +4,7 @@ public final class CategoryPickerOptionsIntent: OnSelectModifier {
   public typealias Model = CategoryPickerOptionsModel
 
   public weak var model: Model?
-  public var actionOnSelect: () -> Void
+  public var actionOnSelect: (() -> Void)?
 
-  public init() {
-    self.actionOnSelect = {}
-  }
+  public init() {}
 }
