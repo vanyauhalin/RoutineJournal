ba920755bdd04464800e9512374bcc72860478d1
vanyauhalin <vanyauhalin@gmail.com>
2022-12-30 16:16:08 +0400

    Add `Foundation` as a dependency for `SettingsView`

--- RoutineJournalSettingsView/Project.swift
+++ RoutineJournalSettingsView/Project.swift
@@ -16,6 +16,7 @@ let project = Project(
       ],
       dependencies: [
         .project("RoutineJournalCategoriesLink"),
+        .project("RoutineJournalFoundation"),
         .project("RoutineJournalUI")
       ]
     )
